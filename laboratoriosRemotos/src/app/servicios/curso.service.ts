1ec62cade7c5dd8d0296b00ce5ca41847e5683ca
Jorge Iván Solano <70910932+jorgeisolano@users.noreply.github.com>
2022-05-27 10:04:30 -0500

    GetMaterias

--- laboratoriosRemotos/src/app/servicios/curso.service.ts
+++ laboratoriosRemotos/src/app/servicios/curso.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, DocumentReference } from '@angular/fire/compat/firestore';
+import { AngularFirestore, DocumentData, DocumentReference } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +7,30 @@ import { AngularFirestore, DocumentReference } from '@angular/fire/compat/firest
 export class CursoService {
 
   col = this.firestr.firestore.collection('Materias');
+  materias: any;
 
   constructor(private firestr: AngularFirestore) { }
 
   deleteFromReference(refCurso: DocumentReference){
     this.firestr.doc(refCurso).delete()
   }
+
+  async getSubjectsFromStudent(refStudent : DocumentData) {
+    const mapMaterias = refStudent["materias"]
+    const refMaterias: any[] = [];
+    
+    for (const key in mapMaterias) {
+      refMaterias.push(mapMaterias[key])
+    }
+
+    refMaterias.forEach(element=>{
+      console.log("Elemento" , element.id)
+      this.col.doc(element.id).get().then(res=>{
+        this.materias.push(res.data())
+      })
+    })
+    console.log("Materias a retornar" , this.materias)
+    return this.materias
+  }
+
 }
