1792408573763d4106216a64dd793fb3a4b90f10
Jorge Iván Solano <70910932+jorgeisolano@users.noreply.github.com>
2022-06-02 20:17:25 -0500

    Cards y mas

--- laboratoriosRemotos/src/app/student-dashboard/subjects/subjects.component.ts
+++ laboratoriosRemotos/src/app/student-dashboard/subjects/subjects.component.ts
@@ -1,3 +1,5 @@
+import { UsuarioService } from './../../servicios/usuario.service';
+import { CursoService } from 'src/app/servicios/curso.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectsComponent implements OnInit {
 
-  constructor() { }
+  materias: any[] = [];
+  constructor(private userService: UsuarioService, private subjectService: CursoService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const id = localStorage.getItem('idUsuario')
+    if (id != null) {
+      const doc = await this.userService.getUser(id)
+      if (doc != undefined) {
+        this.subjectService.getSubjectsFromStudent(doc).then(res => {
+          //alert("obtuvo algo")
+          this.materias = res
+        }).catch(e => {
+          console.log("Error", e)
+        })
+      }
+    }
   }
-
 }
