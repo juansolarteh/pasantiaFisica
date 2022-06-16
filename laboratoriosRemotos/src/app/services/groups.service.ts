056b5e31ed066bf8584f31092ff7955747ce6ebb
juansolarteh <50431375+juansolarteh@users.noreply.github.com>
2022-06-16 00:34:56 -0500

    Revert "traer actualizaciones"

--- laboratoriosRemotos/src/app/services/groups.service.ts
+++ laboratoriosRemotos/src/app/services/groups.service.ts
@@ -1,8 +1,8 @@
-import { ObjectDB } from './../models/ObjectDB';
 import { Injectable } from '@angular/core';
 import { AngularFirestore, DocumentReference } from '@angular/fire/compat/firestore';
 import { Group } from '../models/Group';
 import { MemberGroup } from '../models/MemberGroup';
+import { ObjectDB } from '../models/ObjectDB';
 
 @Injectable({
   providedIn: 'root'
@@ -102,14 +102,5 @@ export class GroupsService {
 
   deleteGroupMember(member: MemberGroup, idSubject: string) {
 
-  }
-
-  async getGroupsByRefStudent(studentRef: DocumentReference){
-    let idGroups : string[] = []
-    let querySnapShot = this.col.where('grupo','array-contains',studentRef).get();
-    (await querySnapShot).forEach(doc=>{
-      idGroups.push(doc.id)
-    })
-    return idGroups
   }
 }
--- laboratoriosRemotos/src/app/student-dashboard/practice/practice.component.ts
+++ laboratoriosRemotos/src/app/student-dashboard/practice/practice.component.ts
@@ -1,9 +1,4 @@
-import { Subject } from 'src/app/models/Subject'
-import { SubjectService } from 'src/app/services/subject.service';
-import { Practice } from 'src/app/models/Practice';
 import { Component, OnInit } from '@angular/core';
-import { ObjectDB } from 'src/app/models/ObjectDB';
-import { PracticeService } from 'src/app/services/practice.service';
 
 @Component({
   selector: 'app-practice',
@@ -12,13 +7,9 @@ import { PracticeService } from 'src/app/services/practice.service';
 })
 export class PracticeComponent implements OnInit {
 
-  constructor(private practiceSvc: PracticeService, private subjectSvc : SubjectService) { }
-  practiceSelected!: ObjectDB<Practice>
-  subjectSelected!: ObjectDB<Subject>
+  constructor() { }
 
   ngOnInit(): void {
-    this.practiceSelected = this.practiceSvc.getPracticeSelected()
-    this.subjectSelected = this.subjectSvc.getSubjectSelected()
   }
 
 }
