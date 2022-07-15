a249d1f6b7d21b94acb7b2df2773efdddab5efa9
juansolarteh <juanpablosh@unicauca.edu.co>
2022-07-15 12:06:06 -0500

    arreglado

--- laboratoriosRemotos/src/app/resolvers/practices-resolver-service.resolver.ts
+++ laboratoriosRemotos/src/app/resolvers/practices-resolver-service.resolver.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { DocumentReference } from '@angular/fire/compat/firestore';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve } from '@angular/router';
 import { SubjectService } from '../services/subject.service';
 import { PracticeService } from '../services/practice.service';
 import { ObjectDB } from '../models/ObjectDB';
@@ -13,9 +13,8 @@ export class PracticesResolverServiceResolver implements Resolve<ObjectDB<Practi
 
   constructor(private subjectSvc: SubjectService, private practiceSvc: PracticeService) { }
 
-  resolve(route: ActivatedRouteSnapshot): Promise<ObjectDB<PracticeNameDate>[]> {
-    const subjectId = route.parent?.paramMap.get('subjectId')!;
-    let subjectRef: DocumentReference = this.subjectSvc.getRefSubjectFromId(subjectId!);
+  resolve(): Promise<ObjectDB<PracticeNameDate>[]> {
+    let subjectRef: DocumentReference = this.subjectSvc.getRefSubjectSelected();
     return this.practiceSvc.getPracticesNameDate(subjectRef);
   }
 }
